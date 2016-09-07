49c4ae6a1de35c00f1aff68b32099d033b4814ea
Sathwik Singari <sathwik@newtonsit.com.au>
2016-09-07 12:50:03 +1000

    Added customer model and serializer

---
+++ accounts/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Customer
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Customer
+        fields = '__all__'
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import User
 
 # Create your views here.
+
+
+@api_view(['POST'])
+def register(request):
+    pass
