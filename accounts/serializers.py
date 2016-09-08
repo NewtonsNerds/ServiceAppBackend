3ea6ad2442a51a183c8dc3c7af722e2c6db20d21
ronak <ronakmpatel@live.com>
2016-09-08 15:19:43 +1000

    Login Webservice

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -84,6 +84,8 @@ class UserLoginSerializer(serializers.ModelSerializer):
 
     id = serializers.IntegerField(required=False, read_only=True)
     token = serializers.CharField(allow_blank=True, read_only=True)
+    mobile = serializers.CharField(required=False, read_only=True)
+    address = serializers.CharField(required=False, read_only=True)
 
     class Meta:
         model = CustomUser
@@ -96,29 +98,32 @@ class UserLoginSerializer(serializers.ModelSerializer):
             'token'
         ]
 
+        validators = []
         extra_kwargs = {
             "password": {
                 "write_only": True
             }
         }
 
-        def validate(self, attrs):
-            email = attrs['email']
-            password = attrs['password']
-            user = CustomUser.objects.get(email=email)
-            if user is None:
-                raise ValidationError("This email is not valid.")
-            else:
-                if not user.check_password(password):
-                    raise ValidationError("Incorrect credentials. Please try again.")
+    def validate(self, attrs):
+        email = attrs['email']
+        password = attrs['password']
+        user = CustomUser.objects.get(email=email)
+        if user is None:
+            raise ValidationError("This email is not valid.")
+        else:
+            if not user.check_password(password):
+                raise ValidationError("Incorrect credentials. Please try again.")
 
-            payload = jwt_payload_handler(user)
+        payload = jwt_payload_handler(user)
 
-            user.last_login = timezone.now()
-            user.save(update_fields=['last_login'])
-            attrs["id"] = user.id
-            attrs["token"] = jwt_encode_handler(payload)
-            return attrs
+        user.last_login = timezone.now()
+        user.save(update_fields=['last_login'])
+        attrs["id"] = user.id
+        attrs["token"] = jwt_encode_handler(payload)
+        attrs["address"] = user.address
+        attrs["mobile"] = user.mobile
+        return attrs
 
 
 class CustomJSONWebTokenSerializer(JSONWebTokenSerializer):
--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.login, name='login'),
     url(r'^(?P<pk>[0-9]+)/', views.detail, name='detail'),
     url(r'^auth/token/', views.GetJSONWebToken.as_view(), name='auth_token'),
 ]
