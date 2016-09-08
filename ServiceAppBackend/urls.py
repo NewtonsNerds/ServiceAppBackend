131f9d4ab53d1153513ee6c299720ced938baa2d
Sathwik Singari <sathwik@newtonsit.com.au>
2016-09-08 12:47:01 +1000

    added JWT - rest framework

--- ServiceAppBackend/urls.py
+++ ServiceAppBackend/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from rest_framework_jwt.views import obtain_jwt_token
+
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
+    url(r'^api/auth/token/', obtain_jwt_token),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
 ]
