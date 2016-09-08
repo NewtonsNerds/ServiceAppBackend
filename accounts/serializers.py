815dcf08f14ab97a1e988b77d7da2634c6a7d2f9
Sathwik Singari <sathwik@newtonsit.com.au>
2016-09-08 10:58:14 +1000

    Added UserLoginSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from .models import CustomUser
 
+from django.utils import timezone
+
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
@@ -48,3 +50,41 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         except CustomUser.DoesNotExist:
             return attrs
         raise ValidationError("This email is already exist")
+
+
+class UserLoginSerializer(serializers.ModelSerializer):
+
+    id = serializers.IntegerField(required=False, read_only=True)
+    token = serializers.CharField(allow_blank=True, read_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = [
+            'id',
+            'email',
+            'mobile',
+            'password',
+            'token'
+        ]
+
+        extra_kwargs = {
+            "password": {
+                "write_only": True
+            }
+        }
+
+        def validate(self, attrs):
+            email = attrs['email']
+            password = attrs['password']
+            user = CustomUser.objects.get(email=email)
+            if user is None:
+                raise ValidationError("This email is not valid.")
+            else:
+                if not user.check_password(password):
+                    raise ValidationError("Incorrect credentials. Please try again.")
+            user.last_login = timezone.now()
+            user.save(update_fields=['last_login'])
+            attrs["id"] = user.id
+            attrs["mobile"] = user.mobile
+            attrs["token"] = "SOME RANDOM TOKEN"
+            return attrs
